f9866f2b79b549d78e42d13848a80ece3bb4959d
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-10-03 20:27:09 -0300

    yargs, regex

--- bin/filesTest/prueba.md
+++ bin/filesTest/prueba.md
@@ -1,2 +1,11 @@
- **DOCUMENTO DE PRUEBA**
+**DOCUMENTO DE PRUEBA**
+
 <link https://www.google.cl></link>
+<link https://www.facebook.com></link>
+<link https://www.twitter.com></link>
+<link https://www.instagram.com></link>
+<link https://www.yahoo.com></link>
+<link https://www.lun.cl></link>
+<link http://www.columbia.edu/~fdc/sample.html></link> 
+<a href=www.example.com/1234 5678>
+<a href=www.example/12345678>
--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,68 @@
 #! /usr/bin/env node  
 // shebang line
+//./bin/index.js
 //basic CLI check!
 
 //development of npm scraper
 
-//carpeta donde se están guardando los archivos
-const filesTest = "./filesTest/";
-
 //NPM'S
-let fs = require("fs");
-const path = require("path");
 //const chalk = require("chalk");
 //const emoji = require("node-emoji");
-const dirPath = path.join(__dirname, filesTest)
+let fs = require("fs");
+const path = require("path");
+const argv = require("yargs")
+.usage("Count the links in a file.\nUsage: $0")
+.example("$0 -f", "count the links in the given file")
+.demand("f")
+.alias("f", "file")
+.describe("f", "Load a file").argv;
 
-//comprobar si la ruta es absoluta o relativa
-if (path.isAbsolute(dirPath)) {
-  //mostrar los archivos que hay en la carpeta
-fs.readdir(dirPath, (err, files) => {
-  if (err) {
-    console.log("error: ", err);
-  } else {
-    files.forEach((file) => {
-      console.log(file);
-      //leer los archivos específicos que terminen en .md
-      if (path.extname(file) === ".md") {
-         fs.readFile(dirPath + file, "utf-8", (err, data) => {
-           if (err) {
-             console.log("error: ", err);
-           } else {
-             console.log(data);
-           }
-         });
-      } else if (path.extname(file) !== ".md") {
-        return false
+let s = fs.createReadStream(argv.file);
+
+  //aceptar parametros segun el usuario
+  //carpeta donde se están guardando los archivos
+  const filesTest = "./filesTest/";
+  const dirPath = path.join(__dirname, filesTest);
+  //comprobar si la ruta es absoluta o relativa
+  if (path.isAbsolute(dirPath)) {
+    //mostrar los archivos que hay en la carpeta
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        console.log("error: ", err);
+      } else {
+        files.forEach((file) => {
+          console.log(file);
+          //leer los archivos específicos que terminen en .md
+          if (path.extname(file) === ".md") {
+            fs.readFile(dirPath + file, "utf-8", (err, data) => {
+              if (err) {
+                console.log("error: ", err);
+              } else {
+                console.log(data);
+                let links = 0;
+                let noValid = 0;
+                s.on("data", function (buf) {
+                  links += buf.toString().match(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/).length;
+                  noValid += !buf.toString().match(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/).length;
+                  });
+                s.on("end", function () {console.log(`${links} links with appropiate safety protocol included in this document \n${noValid} links are not valid.`
+                  );
+                });
+              }
+            });
+          } else if (path.extname(file) !== ".md") {
+            return false;
+          }
+        });
       }
-    })
+    });
+  } else {
+    //transformar la ruta a absoluta si es relativa
+    path.resolve("/Users/enchantress/Desktop/Laboratoria/SCL021-md-links/", dirPath);
   }
-});
-} else {
-  //transformar la ruta a absoluta si es relativa
-  path.resolve('/Users/enchantress/Desktop/Laboratoria/SCL021-md-links/', dirPath)
-}
 
+
+//modularizar (meter todo dentro de una funcion para recibir los argumentos de la usuaria) aceptar los comandos que me de la usuaria (process.args)
+//gestionar la ruta de una carpeta o archivo
+//hacer una peticion y verificar si son correctas
+//http get 
