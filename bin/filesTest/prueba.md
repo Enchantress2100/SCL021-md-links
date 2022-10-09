b2eda7f4374ef045038e5a87ece08a3f52ad0424
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-10-09 20:46:31 -0300

    remake 2.0 aun no funcional

--- bin/filesTest/prueba.md
+++ bin/filesTest/prueba.md
@@ -5,7 +5,5 @@
 <link https://www.twitter.com></link>
 <link https://www.instagram.com></link>
 <link https://www.yahoo.com></link>
-<link https://www.lun.cl></link>
 <link http://www.columbia.edu/~fdc/sample.html></link> 
 <a href=www.example.com/1234 5678>
-<a href=www.example/12345678>
--- bin/index.js
+++ bin/index.js
@@ -6,63 +6,92 @@
 //development of npm scraper
 
 //NPM'S
-//const chalk = require("chalk");
+const colors = require("colors");
 //const emoji = require("node-emoji");
 let fs = require("fs");
 const path = require("path");
-const argv = require("yargs")
-.usage("Count the links in a file.\nUsage: $0")
-.example("$0 -f", "count the links in the given file")
-.demand("f")
-.alias("f", "file")
-.describe("f", "Load a file").argv;
+//ruta relativa.
+let myPath = process.argv[2]
+//ruta absoluta
+const myAbsPath = require("path").resolve();
+//unir argumento con el path
+let route= myPath
 
-let s = fs.createReadStream(argv.file);
+let options = { validate: false, stats: false };
 
-  //aceptar parametros segun el usuario
-  //carpeta donde se están guardando los archivos
-  const filesTest = "./filesTest/";
-  const dirPath = path.join(__dirname, filesTest);
-  //comprobar si la ruta es absoluta o relativa
-  if (path.isAbsolute(dirPath)) {
-    //mostrar los archivos que hay en la carpeta
-    fs.readdir(dirPath, (err, files) => {
+
+//determinar si la ruta ingresada es absoluta, si no, transformarla.
+function mdLinks(route, options) {
+  let arrValidate = [];
+  if (route.isAbsolute) {
+    fs.readdir(route, (err, files) => {
       if (err) {
         console.log("error: ", err);
       } else {
         files.forEach((file) => {
-          console.log(file);
           //leer los archivos específicos que terminen en .md
-          if (path.extname(file) === ".md") {
-            fs.readFile(dirPath + file, "utf-8", (err, data) => {
+          if (route.extname(file) === ".md") {
+            fs.readFile(route + file, "utf-8", (err, data) => {
               if (err) {
                 console.log("error: ", err);
               } else {
                 console.log(data);
-                let links = 0;
-                let noValid = 0;
-                s.on("data", function (buf) {
-                  links += buf.toString().match(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/).length;
-                  noValid += !buf.toString().match(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/).length;
-                  });
-                s.on("end", function () {console.log(`${links} links with appropiate safety protocol included in this document \n${noValid} links are not valid.`
-                  );
-                });
               }
             });
-          } else if (path.extname(file) !== ".md") {
-            return false;
+            //validate -- stats
+            if (myPath === '--validate') {
+              options.validate = true;
+              return response.forEach((element) => {
+                fetch(element.href)
+                  .then((res) => {
+                    arrValidate.push(element);
+
+                    if (res.status === 200) {
+                      return console.log(
+                        `File: ${element.file.magenta} \n Link: ${res.url.cyan} \n Status: ${res.statusText.bgGreen} ${res.status}  ${element.text.yellow}`
+                      );
+                    } else if (res.status === 404) {
+                      return console.log(
+                        ` File: ${element.file.blue} \n Link: ${element.href.cyan}   \n Status: ${res.statusText.bgRed} ${res.status}  ${element.text.yellow}`
+                      );
+                    }
+                  })
+                  .catch((err) => {console.log (`File: ${element.file.blue} \n Link: ${element.href.bgRed} ${element.text.red}  <--- Enlace No Válido o con Problemas`
+                    );
+                  });
+              });
+            }
+          } else if (process.argv[3] === '--stats') {
+            options.stats = true;
+            let urlArray = [];
+            response.forEach(element => {
+              urlArray.push(element.href)
+            })
+            console.log(`El total de links en el archivo ${fileUser.cyan} es: ${urlArray.length}`);
+
+          } else {
+            return response.forEach((element) => {
+              let file = element.file,
+                href = element.href,
+                text = element.text;
+              return console.log(file.blue + ' ' + text.yellow + ' ' + href.green);
+            });
           }
         });
       }
     });
-  } else {
-    //transformar la ruta a absoluta si es relativa
-    path.resolve("/Users/enchantress/Desktop/Laboratoria/SCL021-md-links/", dirPath);
-  }
+  } 
+}
+// console.log('myPath: '+myPath)
+// console.log(`${myAbsPath}`)
+//console.log(route)
+
+//mdLinks(route)
 
 
 //modularizar (meter todo dentro de una funcion para recibir los argumentos de la usuaria) aceptar los comandos que me de la usuaria (process.args)
 //gestionar la ruta de una carpeta o archivo
 //hacer una peticion y verificar si son correctas
 //http get 
+
+console.log(route);
